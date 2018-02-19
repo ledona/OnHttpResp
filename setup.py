4da224cea142a6c7e96a278d4dd7429a25847aca
ledona <delano@ledona.net>
2018-02-19 11:40:32 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='onhttpresp',
+      version='0.1',
+      description='Lightweight abstraction on top of requests',
+      packages=find_packages())
