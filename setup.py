509d07d6eb4fb943bd8a37cbd7cb9dca451ea75f
ledona <delano@ledona.net>
2018-03-19 10:44:31 -0400

    add requirements to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='onhttpresp',
-      version='0.1',
-      description='Lightweight abstraction on top of requests',
-      packages=find_packages())
+setup(
+    name='onhttpresp',
+    version='0.1',
+    description='Lightweight abstraction on top of requests',
+    packages=find_packages(),
+
+    install_requires=(
+        'tqdm',
+        'requests',
+        'sqlalchemy',
+        'pytz',
+    )
+)
