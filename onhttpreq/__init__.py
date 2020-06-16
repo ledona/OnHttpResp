982fed158885de2c9de75e0e22bfba338e0c27d5
ledona <delano@ledona.net>
2020-06-15 20:12:30 -0400

    expose cacheonly exception

--- onhttpreq/__init__.py
+++ onhttpreq/__init__.py
@@ -1,2 +1,2 @@
-from .http_req import HTTPReq, ON_RESPONSE_WAIT_RETRY, ON_RESPONSE_RETURN_WAIT, HTTPReqError, ON_RESPONSE_FAIL
+from .http_req import HTTPReq, ON_RESPONSE_WAIT_RETRY, ON_RESPONSE_RETURN_WAIT, HTTPReqError, ON_RESPONSE_FAIL, CacheOnlyError
 from .cache import CacheOutOfDate, JSONParsingException
