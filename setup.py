0dff266acbdac44dfba1d5a30bb4baef91d66784
delano <delano@ledona.net>
2024-08-13 16:48:06 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="onhttpresp",
-    version="2024.3.29",
+    version="2024.8.13",
     description="Lightweight abstraction on top of requests",
     packages=find_packages(),
     install_requires=("tqdm", "requests", "sqlalchemy", "pytz", "python-dateutil"),
